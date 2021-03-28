f294bd7e964b11237fa382fc2c2b8fa2be96d611
Jonas Lehmann <77493291+jolehmann@users.noreply.github.com>
2021-03-28 21:20:18 +0200

    Changes in Transient and Byzantine failure types.

--- bundles/org.palladiosimulator.failuremodel.edit/src/org/palladiosimulator/failuremodel/failurescenario/provider/OccurrenceItemProvider.java
+++ bundles/org.palladiosimulator.failuremodel.edit/src/org/palladiosimulator/failuremodel/failurescenario/provider/OccurrenceItemProvider.java
@@ -1,12 +1,9 @@
 package org.palladiosimulator.failuremodel.failurescenario.provider;
 
-import java.util.List;
-
 import org.eclipse.emf.common.notify.AdapterFactory;
-import org.eclipse.emf.edit.provider.ComposeableAdapterFactory;
-import org.eclipse.emf.edit.provider.IItemPropertyDescriptor;
-import org.palladiosimulator.failuremodel.failurescenario.FailurescenarioPackage;
 import org.palladiosimulator.failuremodel.failurescenario.Occurrence;
+import org.palladiosimulator.failuremodel.failuretype.Failure;
+import org.palladiosimulator.pcm.core.PCMRandomVariable;
 
 public class OccurrenceItemProvider extends OccurrenceItemProviderGen {
 
@@ -19,45 +16,12 @@ public class OccurrenceItemProvider extends OccurrenceItemProviderGen {
 	 */
 	@Override
 	public String getText(Object object) {
-		final String label = "of " + ((Occurrence) object).getFailure().getEntityName() + " at "
-				+ ((Occurrence) object).getPointInTime().getSpecification();
+		Failure failure = ((Occurrence) object).getFailure();
+		PCMRandomVariable pointInTime = ((Occurrence) object).getPointInTime();
+
+		final String label = ((failure != null) ? "of " + failure.getEntityName() : "")
+				+ ((pointInTime != null) ? " at " + pointInTime.getSpecification() : "");
 		return label == null || label.length() == 0 ? this.getString("_UI_Occurrence_type")
 				: this.getString("_UI_Occurrence_type") + " " + label;
 	}
-	
-	/**
-	 * This returns the property descriptors for the adapted class.
-	 */
-	@Override
-	public List<IItemPropertyDescriptor> getPropertyDescriptors(Object object)
-	{
-		if (itemPropertyDescriptors == null)
-		{
-			super.getPropertyDescriptors(object);
-
-			addFailurePropertyDescriptor(object);
-		}
-		return itemPropertyDescriptors;
-	}
-
-	/**
-	 * This adds a property descriptor for the Failure feature.
-	 */
-	protected void addFailurePropertyDescriptor(Object object)
-	{
-		itemPropertyDescriptors.add
-			(createItemPropertyDescriptor
-				(((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(),
-				 getResourceLocator(),
-				 getString("_UI_Occurrence_failure_feature"),
-				 getString("_UI_PropertyDescriptor_description", "_UI_Occurrence_failure_feature", "_UI_Occurrence_type"),
-				 FailurescenarioPackage.Literals.OCCURRENCE__FAILURE,
-				 true,
-				 false,
-				 true,
-				 null,
-				 null,
-				 null));
-	}
-
 }
